668d039bef7a53d715d69742b8053481e517130e
tommy8808 <tommyguy8808@gmail.com>
2020-09-14 22:01:55 +0900

    todo list의 체크박스를 클릭하여 complete 상태로 변경.

--- TodoApp.js
+++ TodoApp.js
@@ -6,15 +6,16 @@ class TodoApp {
         //TodoList();
         //const todoItem = new TodoItem();
         document.querySelector('#new-todo-title').addEventListener("keydown", this.addTodoItem);
+        document.querySelector('#todo-list').addEventListener("click", this.toggleChecked);
     }
 
     addTodoItem = (e) => {
         if (e.key === "Enter") {
             let ul = document.querySelector('#todo-list');
             let inputValue = document.querySelector('#new-todo-title');
-            let list = `<li>
+            let list = `<li class="completed">
                             <div class="view">
-                                <input class="toggle" type="checkbox"/>
+                                <input class="toggle" checked="true" type="checkbox"/>
                                 <label class="label">${inputValue.value}</label>
                                 <button class="destroy"></button>
                             </div>
@@ -24,7 +25,11 @@ class TodoApp {
         }
     }
 
-    
+    toggleChecked = (e) => {debugger
+        if (e.target.className === "toggle") {
+            e.target.offsetParent.className = e.target.offsetParent.className === "completed" ? "" : "completed";
+        }
+    }
 }
 
 new TodoApp();
