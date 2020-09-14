2c6a699812d2898f50bd0b885741e904a7ae1aa2
tommy8808 <tommyguy8808@gmail.com>
2020-09-14 21:37:39 +0900

    todo list에 todoItem을 키보드로 입력하여 추가하기

--- TodoApp.js
+++ TodoApp.js
@@ -1,12 +1,30 @@
 import {TodoList} from "./components/ToDoList.js"
 import {TodoItem} from "./store/TodoItem.js"
 
-function TodoApp() {
+class TodoApp {
+    constructor(){
+        //TodoList();
+        //const todoItem = new TodoItem();
+        document.querySelector('#new-todo-title').addEventListener("keydown", this.addTodoItem);
+    }
+
+    addTodoItem = (e) => {
+        if (e.key === "Enter") {
+            let ul = document.querySelector('#todo-list');
+            let inputValue = document.querySelector('#new-todo-title');
+            let list = `<li>
+                            <div class="view">
+                                <input class="toggle" type="checkbox"/>
+                                <label class="label">${inputValue.value}</label>
+                                <button class="destroy"></button>
+                            </div>
+                            <input class="edit" value="${inputValue.value}" />
+                        </li>`;
+            ul.innerHTML += list;
+        }
+    }
+
     
-    TodoList();
-    console.log('hello world!!');
-    const todoItem = new TodoItem();
-    todoItem.itemText = "55";
-    console.log(todoItem.itemText);
 }
-TodoApp();
+
+new TodoApp();
