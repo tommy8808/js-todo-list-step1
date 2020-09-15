d14324ce1dae3d4559f3027cd29e63b50622766a
tommy8808 <tommy8808@gmail.com>
2020-09-15 23:38:20 +0900

    두번클릭시 에디터모드, 엔터시 저장까지 구현

--- TodoApp.js
+++ TodoApp.js
@@ -6,16 +6,18 @@ class TodoApp {
         //TodoList();
         //const todoItem = new TodoItem();
         document.querySelector('#new-todo-title').addEventListener("keydown", this.addTodoItem);
-        document.querySelector('#todo-list').addEventListener("click", this.itemEvent);
+        document.querySelector('#todo-list').addEventListener("click", this.listClickEventHandler);
+        document.querySelector('#todo-list').addEventListener("dblclick", this.listDblClickEventHandler);
+        document.querySelector('#todo-list').addEventListener("keydown", this.listKeydownEventHandler);
     }
 
     addTodoItem = (e) => {
         if (e.key === "Enter") {
             let ul = document.querySelector('#todo-list');
             let inputValue = document.querySelector('#new-todo-title');
-            let list = `<li class="completed">
+            let list = `<li class="">
                             <div class="view">
-                                <input class="toggle" checked="true" type="checkbox"/>
+                                <input class="toggle" type="checkbox"/>
                                 <label class="label">${inputValue.value}</label>
                                 <button class="destroy"></button>
                             </div>
@@ -25,13 +27,34 @@ class TodoApp {
         }
     }
 
-    itemEvent = (e) => {
+    listClickEventHandler = (e) => {
         if (e.target.className === "toggle") {
             e.target.offsetParent.className = e.target.offsetParent.className === "completed" ? "" : "completed";
         }
 
         if (e.target.className === "destroy") {
             e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+        }
+    }
+
+    listDblClickEventHandler = (e) => {
+        if (e.target.className === "label") {
+            e.target.offsetParent.className = "editing";
+        }
+    }
+
+    listKeydownEventHandler = (e) => {//이제 state 관리의 필요성 생김
+        if (e.key === "Escape") {//esc 눌렀을때는 원래 값 유지
+            //ex) 
+        }
+        if (e.key === "Enter") {//Enter 시 값 수정
+            const inputValue = e.target.value;
+            //e.target.innerHTML = `<input class="edit" value="${inputValue}">`
+            e.target.offsetParent.querySelector('.label').innerText = inputValue;
+            // const label = document.querySelector('.label');
+            // label.innerText = inputValue;
+            e.target.offsetParent.className = "";
+            //this.setState({e.target: e.target.value})
         }
     }
 }
